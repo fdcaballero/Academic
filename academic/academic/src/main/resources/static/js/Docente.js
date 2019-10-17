dbd080ca6c466d61a5837f30405bd9fd198a0b99
Ana <53981004+anamariabd@users.noreply.github.com>
2019-10-16 23:09:18 -0500

    Add files via upload

---
+++ academic/academic/src/main/resources/static/js/Docente.js
@@ -0,0 +1,40 @@
+$(function(){
+    AddDocente();
+    EliminarDocente();
+    AddButtonDelete();
+});
+
+function AddDocente(){
+   $('#create').on("click", function(event){
+      event.preventDefault();
+      var nombre = $("input[id = nombre]");
+      var Nif = $("input[id = Nif]");
+      var telefono = $("input[id = telefono]");
+      var correo = $("input[id = correo]");
+      $('#prof-tabla').append("<tr class = 'prof'>"+
+      "<th>" + nombre.val() + "</th>" +
+      "<th>" + Nif.val() + "</th>" +
+      "<th>" + telefono.val() + "</th>" +
+      "<th>" + correo.val() + "</th>" +
+      "</tr>");
+       nombre.val('');
+       Nif.val(''); 
+       telefono.val(''); 
+       correo.val('');
+
+   });
+}
+
+function AddButtonDelete(){
+  $('#Delete').on('click', function(event){
+      event.preventDefault();
+      $(".prof").append("<th><a href='#' class='eliminar button'>Eliminar</a></th>");
+  });
+}
+
+function EliminarDocente(){
+    $('#prof-tabla').on('click', '.eliminar', function(event){
+         event.preventDefault();
+         $(this).closest('tr').remove();
+    });
+}
