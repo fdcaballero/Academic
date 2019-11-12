a4f14eeddab048d9f006baed61b25d8ee968ce29
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-12 13:52:07 -0500

    WebMvcConfig

--- academic/academic/src/main/java/edu/unimagdalena/academic/security/WebMvcConfig.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/security/WebMvcConfig.java
@@ -1,5 +1,20 @@
 package edu.unimagdalena.academic.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
 public class WebMvcConfig {
 
+	
+
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
+    }
 }
+
