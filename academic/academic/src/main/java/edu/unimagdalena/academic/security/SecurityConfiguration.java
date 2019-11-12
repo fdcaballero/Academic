baa66ce67259c6cb0e94e3bed1b8148b8cd24d82
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-12 13:50:45 -0500

    SecutiryConfiguration

--- academic/academic/src/main/java/edu/unimagdalena/academic/security/SecurityConfiguration.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/security/SecurityConfiguration.java
@@ -1,19 +1,24 @@
 package edu.unimagdalena.academic.security;
 
 import javax.sql.DataSource;
-/*
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+
 	
 	@Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -39,7 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/?logout").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
+                .accessDeniedPage("/login");
     }
 	
 	
@@ -50,4 +55,3 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**" , "/vendor/**");
     }
 }
-*/
