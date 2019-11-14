eefc1fe98127f768493f3e0d2d680b19a3c9c117
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-14 17:17:12 -0500

    Update RoleRepository.java

--- academic/academic/src/main/java/edu/unimagdalena/academic/repositories/RoleRepository.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/repositories/RoleRepository.java
@@ -5,5 +5,5 @@ import edu.unimagdalena.academic.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
 
-	Role findByNombre(String nombre);
+	Role findByRole(String nombre);
 }
