7d545c0b9b68ffda290671555b9be228b080734d
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-15 10:18:40 -0500

    Update ClaseRepository.java

--- academic/academic/src/main/java/edu/unimagdalena/academic/repositories/ClaseRepository.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/repositories/ClaseRepository.java
@@ -7,5 +7,26 @@ import edu.unimagdalena.academic.entities.Clase;
 
 @Repository
 public interface ClaseRepository  extends JpaRepository<Clase, Long>{
+	@Query("SELECT c FROM Clase c WHERE c.profesor.nombre =: profe")
+	List<Clase> findByProfesor(String profe);
+//	List<Clase> findByCurso(String curso);	
+	List<Clase> findByAsignatura(String asig);	
 
+	@Query("SELECT c FROM Clase c WHERE c.asignatura.nombre =: asig AND c.profesor.nombre =: profe") 
+	List<Clase> findByAsigAndProfe(
+			@Param("asig") String asig, 
+			@Param("profe") String profe);
+    
+/*	@Query("SELECT c FROM Clase c WHERE c.asignatura.curso.etapa =: curso AND c.profesor.nombre =: profe") 
+	List<Clase> findByCursoAndProfe(
+			@Param("curso") String curso, 
+			@Param("profe") String profe);
+    
+	@Query("SELECT c FROM Clase c WHERE c.asignatura.nombre =: asig AND c.asignatura.curso.etapa =: curso") 
+	List<Clase> findByAsigAndCurso(
+			@Param("asig") String asig, 
+			@Param("curso") String curso);
+	*/
+    
+		
 }
