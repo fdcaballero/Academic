ac6bc21e944c967f91f583e1629fe778df556e46
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-15 10:16:43 -0500

    Update ClaseService.java

--- academic/academic/src/main/java/edu/unimagdalena/academic/services/ClaseService.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/services/ClaseService.java
@@ -10,4 +10,10 @@ public interface ClaseService {
 	void delete(Clase clase);
 	List<Clase> findAll();
 	Optional<Clase> findById(Long id);
+	List<Clase> findByAsignatura(String asig);
+//	List<Clase> findByCurso(String curso);
+	List<Clase> findByProfesor(String profe);	
+	List<Clase> findByAsigAndProfe(String asig, String profe);
+//	List<Clase> findByCursoAndProfe(String curso, String profe);
+//	List<Clase> findByAsigAndCurso(String asig, String curso);
 }
