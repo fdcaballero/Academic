9f28937da1353e0565423ac2001829bf0e8219c8
Ana <53981004+anamariabd@users.noreply.github.com>
2019-11-15 10:15:44 -0500

    Update RestClaseController.java

--- academic/academic/src/main/java/edu/unimagdalena/academic/restfull/RestClaseController.java
+++ academic/academic/src/main/java/edu/unimagdalena/academic/restfull/RestClaseController.java
@@ -136,4 +136,41 @@ public class RestClaseController {
 	public List<Clase> getClases(){
 		return classRepositories.findAll();
 	}
+	
+		
+	@GetMapping("/clase02/{asig}/{profe}")
+	public List<Clase> getClaseByAsigAndProfe(@PathVariable String asig, @PathVariable String profe){
+						
+		return classRepositories.findByAsigAndProfe(asig, profe);
+	}
+	
+	@GetMapping("/clase0/{profe}")
+	public List<Clase> getClaseByProfe(@PathVariable String profe){
+						
+		return classRepositories.findByProfesor(profe);
+	}
+	
+/*	@GetMapping("/clase01/{profe}/{curso}")
+	public List<Clase> getClaseByProfeAndCurso(@PathVariable String profe, @PathVariable String curso){
+						
+		return classRepositories.findByCursoAndProfe(curso, profe);
+	}
+	
+	@GetMapping("/clase12/{curso}/{asig}")
+	public List<Clase> getClaseByAsigAndCurso(@PathVariable String asig, @PathVariable String curso){
+						
+		return classRepositories.findByAsigAndCurso(asig, curso);
+	} */
+	
+	@GetMapping("/clase2/{asig}")
+	public List<Clase> getClaseByAsignatura(@PathVariable String asig){
+						
+		return classRepositories.findByAsignatura(asig);
+	}
+	
+/*	@GetMapping("/clase1/{curso}")
+	public List<Clase> getClaseByCurso(@PathVariable String curso){
+						
+		return classRepositories.findByCurso(curso);
+	} */
 }	
